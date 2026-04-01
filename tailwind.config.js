5ebb9d034d8b360db589b9695c8ee7d3d3dcc65a
MishSoft <michaelmakaveli123@gmail.com>
2026-04-01 18:55:46 +0400

    Feat: create costumize styles, add icon, images folders, create structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,23 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        "header-bg": "var(--header-bg)",
+        "item-bg": "var(--item-bg)",
+        "header-button-bg": "var(--header-button-bg)",
+        "item-button-bg": "var(--item-button-bg)",
+        "hero-demo-btn-bg": "var(--hero-demo-btn-bg)",
+        "text-primary-color": "var(--text-primary-color)",
+        "header-text-primary": "var(--header-text-primary)",
+        "header-button-text": "var(--header-button-text)",
+        "hero-text-primary": "var(--hero-text-primary)",
+        "hero-text-secondary": "var(--hero-text-secondary)",
+        "hero-image-shadow-color": "var(--hero-image-shadow-color)",
+      },
+    },
   },
   plugins: [],
-}
+};
 
